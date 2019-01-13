1e74dcbaa7bac37bf9a3cce08fcf4dc245ae61cc
0x64 <623878692@qq.com>
2019-01-13 16:17:27 +0800

    20190113

---
+++ is_reverse.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+int is_reverse(char *s)
+{
+    int i = 0;
+    int len = strlen(s);
+    for(i = 0;i < ((len+1)/2);i++)
+    {
+        if(*(s+i) != *(s+(len-1-i)))
+            return 0;
+    }
+    return 1;
+}
+
+int main(int argc, char const *argv[])
+{
+    char s[10];
+    memset(s, 56, sizeof(s));
+    while(scanf("%s", s) == 1 && *s != '0')
+        if(is_reverse(s))
+            printf("is reverse\n");
+        else
+            printf("is not reverse\n");
+    system("pause");    
+    return 0;
+}
