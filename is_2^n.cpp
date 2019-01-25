400586f0681fe1059cf3681f664750be96807a41
0x64 <623878692@qq.com>
2019-01-25 11:58:16 +0800

    20190125

---
+++ is_2^n.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 8;
+    if ((x & (x - 1)) == 0)
+    {
+        cout << "yes" << endl;
+    }
+    else
+    {
+        cout << "no" << endl;
+    }
+
+    x = 9;
+    if ((x & (x - 1)) == 0)
+    {
+        cout << "yes" << endl;
+    }
+    else
+    {
+        cout << "no" << endl;
+    }
+    system("pause");
+    return 0;
+}
