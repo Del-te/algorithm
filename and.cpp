6fe98e964dc905bac5c2da92d63e43dc02b1b601
0x64 <623878692@qq.com>
2019-01-25 11:48:49 +0800

    20190125

---
+++ and.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 0b1001;
+    int count = 0;
+    for(size_t i = 0; i < 32; i++)
+    {
+        if ((x & (1 << i)) == (1 << i)) {
+            count++;
+        }
+    }
+    cout << x << " " << count << endl;
+    count = 0;
+    for(size_t i = 0; i < 32; i++)
+    {
+        if ((x >> i) & 1 == 1)
+            count++;
+    }
+    cout << x << " " << count << endl;
+    count = 0;
+    int t = x;
+    while(true)
+    {
+        x = x & (x-1);
+        count++;
+        if (x == 0) {
+            break;
+        }
+    }
+    cout << t << " " << count << endl;
+    system("pause");
+    return 0;
+}
+// 二进制数中1的个数
