2773d839ca9b94883af675e2cdf676502c89d3ae
0x64 <623878692@qq.com>
2019-01-27 22:57:02 +0800

    20190127

--- Joseph.c
+++ Joseph.c
@@ -1,16 +1,18 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct Node {
+typedef struct Node
+{
     int data;
     struct Node *next;
-}node;
+} node;
 int len = 0;
 
-node *Create_Node(int i)//新建并初始化节点
+node *Create_Node(int i) //新建并初始化节点
 {
-    node *p = (node *) malloc(sizeof(node));
-    if (!p) {
+    node *p = (node *)malloc(sizeof(node));
+    if (!p)
+    {
         printf("Error,the memory is not enough!\n");
         exit(-1);
     }
@@ -20,11 +22,12 @@ node *Create_Node(int i)//新建并初始化节点
     return p;
 }
 
-void Create_List(node *head, int n)//把其余数据添加到循环单链表中
+void Create_List(node *head, int n) //把其余数据添加到循环单链表中
 {
     int i = 0;
     node *pNew, *p;
-    if(len == 0) {
+    if (len == 0)
+    {
         pNew = Create_Node(2);
         head->data = 1;
         pNew->data = 2;
@@ -33,7 +36,8 @@ void Create_List(node *head, int n)//把其余数据添加到循环单链表中
         pNew->next = NULL;
         len = 2;
     }
-    for (i = 3; i <= n; i++) {
+    for (i = 3; i <= n; i++)
+    {
         pNew = Create_Node(i);
         p->next = pNew;
         pNew->next = NULL;
@@ -43,13 +47,14 @@ void Create_List(node *head, int n)//把其余数据添加到循环单链表中
     p->next = head;
 }
 
-void Print_List(node *head)//输出链表内容
+void Print_List(node *head) //输出链表内容
 {
     node *p = head;
     int i = 1;
     printf("The joseph ring:\n");
-    while(i <= len){
-        if(p)
+    while (i <= len)
+    {
+        if (p)
             printf("%d\n", p->data);
         p = p->next;
         i++;
@@ -57,12 +62,13 @@ void Print_List(node *head)//输出链表内容
     printf("The length of the List:%d\n", len);
 }
 
-void joseph(node *head, int count)//约瑟夫回环函数实现
+void joseph(node *head, int count) //约瑟夫回环函数实现
 {
     node *p1 = head, *p2 = head;
     int i = 0;
-    while (len != 1) {
-        for(i = 0;i < count -1 ;i++)
+    while (len != 1)
+    {
+        for (i = 0; i < count - 1; i++)
             p1 = p1->next;
         p2 = p1->next;
         p1->next = p2->next;
@@ -73,7 +79,8 @@ void joseph(node *head, int count)//约瑟夫回环函数实现
     printf("The last one is:%d\n", p1->data);
 }
 
-int main() {
+int main()
+{
     int n = 0;
     int count = 0;
     printf("Please input the length of the joseph ring:");
