70644eaa22cf5368d3307f5d020934feeada5b4b
0x64 <623878692@qq.com>
2019-01-27 22:57:22 +0800

    20190127

--- ancient_cipher.c
+++ ancient_cipher.c
@@ -5,7 +5,7 @@
 
 char str1[maxn];
 char str2[maxn];
-int count1[26]; 
+int count1[26];
 int count2[26];
 
 int comparator(const void *a, const void *b)
@@ -17,7 +17,7 @@ void count()
     int i = 0, j = 0;
     for (i = 0; i < strlen(str1); i++)
         count1[str1[i] - 'A']++;
-    for(j = 0; j < strlen(str2); j++)
+    for (j = 0; j < strlen(str2); j++)
         count2[str2[j] - 'A']++;
 }
 int judge()
@@ -28,17 +28,17 @@ int judge()
     qsort(count2, 26, sizeof(int), comparator);
     for (i = 0; i < 26; i++)
     {
-        if(count1[i] != count2[i])
+        if (count1[i] != count2[i])
             return 0;
     }
     return 1;
 }
 int main(int argc, char const *argv[])
 {
-    memset(count1, 0, sizeof(int)*26);
-    memset(count2, 0, sizeof(int)*26);
-    memset(str1, 0x00, sizeof(char)*maxn);
-    memset(str2, 0x00, sizeof(char)*maxn);
+    memset(count1, 0, sizeof(int) * 26);
+    memset(count2, 0, sizeof(int) * 26);
+    memset(str1, 0x00, sizeof(char) * maxn);
+    memset(str2, 0x00, sizeof(char) * maxn);
     while (scanf("%s%s", str1, str2) == 2 && str1[0] != '0')
     {
         if (judge())
