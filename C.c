10ab0263ef0956376ed062a7f7f7d69c28945d80
0x64 <623878692@qq.com>
2019-01-31 16:12:39 +0800

    20190131

--- C.c
+++ C.c
@@ -3,19 +3,20 @@
 
 long long C(int n, int m)
 {
-    long long ans = 1;  // 不能等于0，0乘以任何数都为0！
+    long long ans = 1; // 不能等于0，0乘以任何数都为0！
     int i = 0;
-    if (m < n-m) m = n - m;
-    for(i = m+1; i <= n; i++)
+    if (m < n - m)
+        m = n - m;
+    for (i = m + 1; i <= n; i++)
         ans *= i;
-    for (i = 1; i <= n-m; i++)
+    for (i = 1; i <= n - m; i++)
         ans /= i;
     return ans;
 }
 int main(int argc, char const *argv[])
 {
     int n, m;
-    while(scanf("%d%d", &n,&m) == 2 && (n != 0 && m != 0))
+    while (scanf("%d%d", &n, &m) == 2 && (n != 0 && m != 0))
         printf("%lld\n", C(n, m));
     system("pause");
     return 0;
