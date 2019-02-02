054069ad38895264e2dacd0ad811d7781b52be48
0x64 <623878692@qq.com>
2019-02-02 15:30:28 +0800

    20190202

--- biginterger.cpp
+++ biginterger.cpp
@@ -8,28 +8,36 @@ struct BigInteger
     static const int WIDTH = 8;
     vector<int> s;
 
-    BigInteger(long long num = 0) { *this = num; } // 构造函数
+    BigInteger(long long num = 0)
+    {
+        *this = num;
+    }
     BigInteger operator=(long long num)
-    { // 赋值运算符
+    {
         s.clear();
         do
         {
+            // num的后八位
             s.push_back(num % BASE);
+            // 把num的后八位删除
             num /= BASE;
         } while (num > 0);
+        // 返回对象本身
         return *this;
     }
-    BigInteger operator=(const string &str)
-    { // 赋值运算符
+    BigInteger operator=(const string &str) // 传引用避免不必要的复制
+    {
         s.clear();
         int x;
         // 若有32位，分成(32-1)/8+1=4块；
         // 若有31位，分成(31-1)/8+1=4块；
         // 若有33位，分成(33-1)/8+1=5块，所有情况通吃，好技巧。
-        int len = (str.length() - 1) / WIDTH + 1;
-        for (int i = 0; i < len; i++)
+        // 共有几块
+        int len = (str.length() - 1) / WIDTH + 1; // 分块技巧
+        for (size_t i = 0; i < len; i++)
         {
-            int end = str.length() - i * WIDTH;
+            // 由start 和 end 取每八位
+            int end = str.length() - i * WIDTH; // 分块技巧
             int start = max(0, end - WIDTH);
             // substr()获得字符串s中 从第 位开始的长度为  的字符串
             // 默认时的长度为从开始位置到尾
@@ -45,51 +53,57 @@ struct BigInteger
     }
     BigInteger operator+(const BigInteger &b) const
     {
-        BigInteger c;
-        c.s.clear();
-        for (int i = 0, g = 0;; i++)
+        // 新声明一个用来存结果
+        BigInteger result;
+        result.s.clear();
+        for (size_t i = 0, g = 0;; i++)
         {
             if (g == 0 && i >= s.size() && i >= b.s.size())
                 break;
             int x = g;
+            // x = a.s[i]
             if (i < s.size())
                 x += s[i];
+            // x = a.s[i] + b.s[i]
             if (i < b.s.size())
                 x += b.s[i];
-            c.s.push_back(x % BASE);
+            result.s.push_back(x % BASE);
             g = x / BASE;
         }
-        return c;
+        return result;
     }
 };
-
 ostream &operator<<(ostream &out, const BigInteger &x)
 {
+    // 跳过可能不足八位的那个
     out << x.s.back();
     for (int i = x.s.size() - 2; i >= 0; i--)
     {
         char buf[20];
         sprintf(buf, "%08d", x.s[i]);
         for (int j = 0; j < strlen(buf); j++)
+        {
             out << buf[j];
+        }
     }
     return out;
 }
-
 istream &operator>>(istream &in, BigInteger &x)
 {
     string s;
-    if (!(in >> s))
+    if (!(in >> s)) {
         return in;
+    }
     x = s;
     return in;
 }
-
 int main()
 {
-    BigInteger a;
+    BigInteger a, b;
     a = "1111111111";
-    a = a + a;
+    cin >> b;
+    a = a + b;
+    cout << a;
     system("pause");
     return 0;
 }
