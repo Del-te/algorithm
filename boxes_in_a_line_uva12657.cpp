c7e6b9642ddeb1f2076fb00ebf9246bbe7a88d79
0x64 <623878692@qq.com>
2019-02-13 18:56:15 +0800

    20190213

--- boxes_in_a_line_uva12657.cpp
+++ boxes_in_a_line_uva12657.cpp
@@ -22,8 +22,10 @@ int main()
             left[i] = i - 1;
             right[i] = (i + 1) % (n + 1); // 求余n+1避免越界
         }
+        // 让模拟链表循环
         right[0] = 1;
         left[0] = n;
+
         int op, X, Y, inv = 0;
 
         while (m--)
@@ -36,13 +38,16 @@ int main()
                 scanf("%d%d", &X, &Y);
                 if (op == 3 && right[Y] == X)
                     swap(X, Y);
+                // 如果是12操作并且已经进行过奇数次4操作，那么12操作要掉个
                 if (op != 3 && inv)
                     op = 3 - op;
+                // 忽略无需进行的操作，减少开支
                 if (op == 1 && X == left[Y])
                     continue;
                 if (op == 2 && X == right[Y])
                     continue;
-
+                // 进行12操作
+                // x的左边 x的右边 y的左边 y的右边
                 int LX = left[X], RX = right[X], LY = left[Y], RY = right[Y];
                 if (op == 1)
                 {
@@ -79,10 +84,11 @@ int main()
         long long ans = 0;
         for (int i = 1; i <= n; i++)
         {
-            b = right[b];
-            if (i % 2 == 1)
-                ans += b;
+            b = right[b]; // 向右移动
+            if (i % 2 == 1) // 如果是奇数次
+                ans += b; // 所有奇数位置编号之和
         }
+        // 如果进行过4操作并且n是偶数，ans用总的编号之和减去奇数编号之和
         if (inv && n % 2 == 0)
             ans = (long long)n * (n + 1) / 2 - ans;
         printf("Case %d: %lld\n", ++kase, ans);
