94ca8bac7973aebefdd2f74bed6ac075c1ab649c
0x64 <623878692@qq.com>
2019-02-23 22:36:17 +0800

    20190223

--- rodering_tasks_uva10306.cpp
+++ rodering_tasks_uva10306.cpp
@@ -4,22 +4,24 @@
 #include <cstring>
 const int maxn = 1000;
 // g[i][j]表示i要先于j
-int n, m, G[maxn][maxn], c[maxn], topo[maxn], t;
 // m表示关系数量，n表示有多少个任务
+// flag是标记数组，0表示未访问，-1表示正在访问，1表示已经访问
+// topo是拓扑序列结果
+int n, m, G[maxn][maxn], flag[maxn], topo[maxn], t;
 bool dfs(int u)
 {
   // -1表示正在访问
-  c[u] = -1;
+  flag[u] = -1;
   for (int v = 0; v < n; v++)
     if (G[u][v])
     {
-      if (c[v] < 0)
+      if (flag[v] < 0)
         return false;
-      else if (!c[v])
+      else if (!flag[v])
         dfs(v);
     }
   // 1表示访问过了
-  c[u] = 1;
+  flag[u] = 1;
   topo[--t] = u;
   return true;
 }
@@ -27,9 +29,10 @@ bool dfs(int u)
 bool toposort()
 {
   t = n;
-  memset(c, 0, sizeof(c));
+  // 标记数组初始化
+  memset(flag, 0, sizeof(flag));
   for (int u = 0; u < n; u++)
-    if (!c[u])
+    if (!flag[u]) // 如果未被访问过
       if (!dfs(u))
         return false;
   return true;
@@ -49,6 +52,7 @@ int main()
       v--;
       //因为题意是从1开始的节点，所以存储进去的时候必须减一
       G[u][v] = 1;
+      // 表示u偏序v关系成立
     }
     if (toposort())
     {
