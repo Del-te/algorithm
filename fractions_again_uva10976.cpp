f637373a0fbc478597fe5fe122d02ff1d966339b
0x64 <623878692@qq.com>
2019-02-26 20:20:43 +0800

    20190226

--- fractions_again_uva10976.cpp
+++ fractions_again_uva10976.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <vector>
 #include <cstdlib>
 using namespace std;
 int main()
@@ -6,18 +7,24 @@ int main()
     int k;
     while (scanf("%d", &k) == 1 && k)
     {
+        vector<int> X, Y;
         for (int y = k + 1; y <= 2 * k; y++)
         {
             double x = 0;
             if (y != k)
                 x = (double)k * y / (double)(y - k);
+            // if( k * y % (y - k) == 0)
             // 判断一个浮点数是否为整数
             // 浮点数不可用 == 和！=来比较，因为存在误差！
             if ((int)x >= y && (x - (int)x < 0.000000001) && (x - (int)x > -0.000000001))
             {
-                printf("1/%d = 1/%d + 1/%d\n", k, (int)x, y);
+                X.push_back((int)x);
+                Y.push_back(y);
             }
         }
+        printf("%d\n", X.size());
+        for (size_t i = 0; i < X.size(); i++)
+            printf("1/%d = 1/%d + 1/%d\n", k, X[i], Y[i]);
     }
     system("pause");
     return 0;
