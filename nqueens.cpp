4d2948793507a475c81550c401865df72cd9e4b1
0x64 <623878692@qq.com>
2019-03-07 22:59:38 +0800

    20190307

--- nqueens.cpp
+++ nqueens.cpp
@@ -8,12 +8,14 @@ int N;
 int count = 0;
 void tracing_back(int cur)
 {
+    // 要把0到7行全判断完才能算一个解
     if (cur >= N)
     {
         count++;
     }
     else for (int i = 0; i < N; i++)
     {
+        // 先判断有没有走过，再进行操作
         if (!vis[0][i] && !vis[1][cur + i] && !vis[2][cur - i + N])
         {
             vis[0][i] = vis[1][cur + i] = vis[2][cur - i + N] = 1;
@@ -27,6 +29,7 @@ int main()
     int cur = 0;
     cin >> N;
     memset(vis, 0, sizeof(vis));
+    // 从第0行开始
     tracing_back(cur);
     cout << count << endl;
     system("pause");
