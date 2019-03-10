bcb39081d61e26701320ad2d4be48f43e9ff3f3b
0x64 <623878692@qq.com>
2019-03-10 23:04:20 +0800

    20190310

--- mergr_sort.cpp
+++ mergr_sort.cpp
@@ -6,22 +6,22 @@ void merge_sort(int *A, int x, int y, int *T)
     if (y - x > 1)
     {
         int m = x + (y - x) / 2;
-        int p = x, q = m, i = x;
+        int p1 = x, p2 = m, i = x;
         merge_sort(A, x, m, T);
         merge_sort(A, m, y, T);
         // 巧用短路表达式
-        while (p < m || q < y)
+        while (p1 < m || p2 < y)
         {
-            if (q >= y || (p < m && A[p] <= A[q]))
+            if (p2 >= y || (p1 < m && A[p1] <= A[p2]))
             {
                 // 复制后移动下标
                 // 把左边赋给替换数组
-                T[i++] = A[p++]; 
+                T[i++] = A[p1++]; 
             }
             else
             {
                 // 把右边赋给替换数组
-                T[i++] = A[q++];
+                T[i++] = A[p2++];
             }
         }
         // 把替换数组赋回去
