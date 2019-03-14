c34cd80e058acbc57cc505fb97997703286adf3d
0x64 <623878692@qq.com>
2019-03-14 18:15:00 +0800

    20190314

---
+++ stacks_of_flapjacks_uva120.cpp
@@ -0,0 +1,36 @@
+#include<cstdio>
+#include<iostream>
+#include<sstream>
+#include<algorithm>
+using namespace std;
+
+const int maxn = 30 + 5;
+int n, a[maxn];
+
+// 翻转a[0..p]
+void flip(int p) {
+  // i小于二分之p
+  for(int i = 0; i < p-i; i++) 
+    swap(a[i], a[p-i]);
+  printf("%d ", n-p);
+}
+
+int main() {
+  string s;
+  while(getline(cin, s)) {
+    cout << s << "\n";
+    stringstream ss(s);
+    n = 0;
+    // 从上到下
+    while(ss >> a[n]) n++;
+    for(int i = n-1; i > 0; i--) {
+      // 元素a[0..i]中的最大元素的位置
+      int p = max_element(a, a+i+1) - a; // max_element返回的是指针 
+      if(p == i) continue;
+      if(p > 0) flip(p); // flip(0)没啥意思，是不？
+      flip(i);
+    }
+    printf("0\n");
+  }
+  return 0;
+}
