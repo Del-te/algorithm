cc6cf88b5b448dac4e51fb7cacd77066a20de92b
0x64 <623878692@qq.com>
2019-03-15 22:15:04 +0800

    20190315

---
+++ wine_trading_in_gergovia_uva11054.cpp
@@ -0,0 +1,21 @@
+// ???
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    while (cin >> n && n)
+    {
+        long long ans = 0, a, last = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a;
+            ans += abs(last);
+            last += a;
+        }
+        cout << ans << "\n";
+    }
+    return 0;
+}
