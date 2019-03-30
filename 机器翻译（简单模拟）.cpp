4ba7b7c1cbe8b89c94694978149ca82ec9dfef60
0x64 <623878692@qq.com>
2019-03-30 16:33:28 +0800

    20190330

--- 机器翻译（简单模拟）.cpp
+++ 机器翻译（简单模拟）.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 const int maxn = 1005;
 int a[maxn];
+
+// void debug(vector<int> mem)
+// {
+//     vector<int>::iterator i = mem.begin();
+//     for(i = mem.begin(); i != mem.end(); i++)
+//     {
+//         cout << *i << endl;
+//     }
+//     cout << "size:" << mem.size() << endl;
+// }
 int main()
 {
     vector<int> mem;
@@ -13,35 +23,38 @@ int main()
     {
         cin >> a[i];
     }
+
     for (int j = 0; j < n; j++)
     {
+        // 内存中是否已存在该值
         bool flag = false;
-        vector<int>::iterator k = mem.begin();
-        for (k = mem.begin(); k != mem.end(); k++)
+        // 先查一遍有没有a[j]
+        if (mem.size() != 0)
         {
-            if (*k == a[j])
+            vector<int>::iterator k = mem.begin();
+            for (k = mem.begin(); k != mem.end(); k++)
             {
-                flag = true;
-                break;
+                if (*k == a[j])
+                {
+                    flag = true;
+                    break;
+                }
             }
         }
-
-        if (mem.size() < 3 || flag == false)
+        // 当储存个数小于三个并且a[j]未存入时直接压入
+        if (mem.size() < m && flag == false)
         {
-
             mem.push_back(a[j]);
             cnt++;
-            cout << "*l:" << mem[0] << endl;
-            cout << "size:" << mem.size() << endl;
+            // debug(mem);
         }
-
-        vector<int>::iterator l = mem.begin();
-        if (flag == false)
+        // 储存个数大于三个时如果a[j]未存入
+        else if (flag == false)
         {
+            vector<int>::iterator l = mem.begin();
             mem.erase(l);
             mem.push_back(a[j]);
-            cout << "*l:" << *l << endl;
-            cout << "size:" << mem.size() << endl;
+            // debug(mem);
             cnt++;
         }
     }
