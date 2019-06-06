ba6efe90fa97a2c8a6eb3bb495eb6512a43d6bfe
0x64 <623878692@qq.com>
2019-06-06 15:55:49 +0800

    20190606

--- ZJU1027.cpp
+++ ZJU1027.cpp
@@ -1,21 +1,18 @@
+// @Author 贾立威 201710405130
 #include <cstdio>
 #include <iostream>
 #include <cstdlib>
 using namespace std;
 const int MaxN = 101;
 
-// 两个基因序列
-char str1[MaxN], str2[MaxN];
-// 记录动态规划过程中产生的中间结果
-// gene[i][j]表示基因子串str1[0...i-1]和str2[0...j-1]的分值
-int gene[MaxN][MaxN];
 // 基因分值矩阵
-int score[5][5] = {{5, -1, -2, -1, -3},
-                   {-1, 5, -3, -2, -4},
-                   {-2, -3, 5, -2, -2},
-                   {-1, -2, -2, 5, -1},
-                   {-3, -4, -2, -1, 0}};
+const int score[5][5] = {{5, -1, -2, -1, -3},
+                         {-1, 5, -3, -2, -4},
+                         {-2, -3, 5, -2, -2},
+                         {-1, -2, -2, 5, -1},
+                         {-3, -4, -2, -1, 0}};
 
+// 求三个数最大值
 int get_max(int num1, int num2, int num3)
 {
     int result = num1;
@@ -26,6 +23,7 @@ int get_max(int num1, int num2, int num3)
     return result;
 }
 
+// 将AGCT-转化为整数进行处理
 int convert(char c)
 {
     switch (c)
@@ -46,8 +44,16 @@ int convert(char c)
 
 int main()
 {
+    // 每组测试用例数量
     int n;
+    // 两个基因序列
+    char str1[MaxN], str2[MaxN];
+    // 第一个序列的长度，第二个序列的长度
     int len_a, len_b;
+    // 记录动态规划过程中产生的中间结果
+    // gene[i][j]表示基因子串str1[0...i-1]和str2[0...j-1]的分值
+    int gene[MaxN][MaxN];
+
     cin >> n;
     for (int k = 0; k < n; k++)
     {
@@ -55,18 +61,24 @@ int main()
         cin >> len_b >> str2;
 
         gene[0][0] = 0;
-        for (int i = 1; i <= len_b; i++)
-            gene[0][i] = gene[0][i - 1] + score[4][convert(str2[i - 1])];
         for (int i = 1; i <= len_a; i++)
+            // 第一条序列碱基仅匹配‘-’时
             gene[i][0] = gene[i - 1][0] + score[convert(str1[i - 1])][4];
+        for (int i = 1; i <= len_b; i++)
+            // 第二条序列碱基仅匹配‘-’时
+            gene[0][i] = gene[0][i - 1] + score[4][convert(str2[i - 1])];
+
         int m1, m2, m3;
         for (int i = 1; i <= len_a; i++)
             for (int j = 1; j <= len_b; j++)
             {
-                // 三种情况
-                m1 = gene[i - 1][j] + score[convert(str1[i - 1])][4];
+                // 第一条序列碱基仅匹配‘-’时 
+                m1 = gene[i - 1][j] + score[convert(str1[i - 1])][4]; 
+                // 第二条序列碱基仅匹配‘-’时
                 m2 = gene[i][j - 1] + score[4][convert(str2[j - 1])];
+                // 两条序列碱基相互匹配时
                 m3 = gene[i - 1][j - 1] + score[convert(str1[i - 1])][convert(str2[j - 1])];
+                // 选取最优情况
                 gene[i][j] = get_max(m1, m2, m3);
             }
         cout << gene[len_a][len_b] << endl;
